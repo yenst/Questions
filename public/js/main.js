d9abb9f6cb92db34111880779f75c41ac7b5ce24
Quentin <quentin.bultinck@student.howest.be>
2017-11-29 15:18:07 +0100

    Detail Thread Page

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,15 @@ let newClass = false;
 let images = [];
 
 const gInterface = (function () {
-    //TODO use this function with callbacks
-    let askToLogin = function () {
-        gInterface.showError("Please login to continue.");
+
+    let isAuthenticated = function () {
+        return new Promise(function (resolve) {
+            if (socketModule.isConnected()) {
+                resolve();
+            } else {
+                gInterface.showError("Please login to continue.");
+            }
+        })
     };
 
     return {
@@ -16,7 +22,7 @@ const gInterface = (function () {
             $("#question_form")
                 .on("submit", function (e) {
                     e.preventDefault();
-                    if (socketModule.isConnected()) {
+                    isAuthenticated().then(() => {
                         let $questionInput = $(e.target).find("#question");
                         let $pollChoiceList = $("#pollChoices");
                         let question = $questionInput.val();
@@ -38,7 +44,7 @@ const gInterface = (function () {
                         $pollChoiceList.html("");
                         $(this).find("input[name='choice']").val("");
                         self.initAutoComplete();
-                    } else askToLogin();
+                    });
                 })
                 .on("click", ".removeSelfOnClick", function (e) {
                     e.preventDefault();
@@ -51,30 +57,31 @@ const gInterface = (function () {
             });
             $("#answer_form").on("submit", function (e) {
                 e.preventDefault();
-                if (socketModule.isConnected()) {
+                isAuthenticated().then(() => {
                     let $answerInput = $(e.target).find("#answer");
                     let $threadIdInput = $(e.target).find("input[name='threadId']");
                     socketModule.sendAnswer($threadIdInput.val(), $answerInput.val(), images);
                     $("#answerFormModal").modal("hide");
                     $answerInput.val("");
                     $threadIdInput.val("");
-                } else askToLogin();
+                });
             });
-            $("#alias_form").on("submit",function(e){
+            $("#alias_form").on("submit", function (e) {
                 $("#aliasFormModal").modal("hide");
                 e.preventDefault();
                 name = $(e.target).find("input[name='alias']").val();
-                $.post("/edit-alias",{
-                    new_name:name
-                },
-            function(data,status){})
-            .done(function(response){
-                window.location.reload();
-            })
+                $.post("/edit-alias", {
+                        new_name: name
+                    },
+                    function (data, status) {
+                    })
+                    .done(function (response) {
+                        window.location.reload();
+                    })
             });
             $("#comment_form").on("submit", function (e) {
                 e.preventDefault();
-                if (socketModule.isConnected()) {
+                isAuthenticated().then(() => {
                     let $commentInput = $(e.target).find("input[name='comment']");
                     let $answerIdInput = $(e.target).find("input[name='answerId']");
                     let $threadIdInput = $(e.target).find("input[name='threadId']");
@@ -87,11 +94,11 @@ const gInterface = (function () {
                     $commentInput.val("");
                     $threadIdInput.val("");
                     $answerIdInput.val("");
-                } else askToLogin();
+                });
             });
             $("#tag_form").on("submit", function (e) {
                 e.preventDefault();
-                if (socketModule.isConnected()) {
+                isAuthenticated().then(() => {
                     let $tagInput = $(e.target).find("input[name='tag']");
                     let $threadIdInput = $(e.target).find("input[name='threadId']");
                     socketModule.addTag(
@@ -102,12 +109,12 @@ const gInterface = (function () {
                     $threadIdInput.val("");
                     $tagInput.val("");
                     self.initAutoComplete();
-                } else askToLogin();
+                });
             });
             $("#threads")
                 .on("click", ".answerButton", function (e) {
                     e.preventDefault();
-                    if (socketModule.isConnected()) {
+                    isAuthenticated().then(() => {
                         let $currentThread = $(e.target).closest(".thread");
                         let threadId = $currentThread.attr("data-thread-id");
                         let threadQuestion = $currentThread.find(".question").text();
@@ -115,7 +122,7 @@ const gInterface = (function () {
                         $answerFormModal.find("input[name='threadId']").attr("value", threadId);
                         $answerFormModal.find(".threadQuestion").text(threadQuestion);
                         $answerFormModal.modal("show");
-                    } else askToLogin();
+                    });
                 })
                 .on("click", ".answersVisibilityToggler", function (e) {
                     e.preventDefault();
@@ -143,7 +150,7 @@ const gInterface = (function () {
                 })
                 .on("click", ".commentButton", function (e) {
                     e.preventDefault();
-                    if (socketModule.isConnected()) {
+                    isAuthenticated().then(() => {
                         let $currentThread = $(e.target).closest(".thread");
                         let $currentAnswer = $(e.target).closest(".answer");
                         let threadId = $currentThread.attr("data-thread-id");
@@ -154,43 +161,43 @@ const gInterface = (function () {
                         let $commentFormModal = $("#commentFormModal");
                         $commentFormModal.find(".threadAnswer").text(answer);
                         $commentFormModal.modal("show");
-                    } else askToLogin();
+                    });
                 })
                 .on("click", ".threadUpVoteBtn", function (e) {
                     e.preventDefault();
-                    if (socketModule.isConnected()) {
+                    isAuthenticated().then(() => {
                         let threadId = $(e.target)
                             .closest(".thread")
                             .attr("data-thread-id");
                         socketModule.upVoteThread(threadId);
-                    } else askToLogin();
+                    });
                 })
                 .on("click", ".threadDownVoteBtn", function (e) {
                     e.preventDefault();
-                    if (socketModule.isConnected()) {
+                    isAuthenticated().then(() => {
                         let threadId = $(e.target)
                             .closest(".thread")
                             .attr("data-thread-id");
                         socketModule.downVoteThread(threadId);
-                    } else askToLogin();
+                    });
                 })
                 .on("click", ".answerUpVoteBtn", function (e) {
                     e.preventDefault();
-                    if (socketModule.isConnected()) {
+                    isAuthenticated().then(() => {
                         let answerId = $(e.target)
                             .closest(".answer")
                             .attr("data-answer-id");
                         socketModule.upVoteAnswer(answerId);
-                    } else askToLogin();
+                    });
                 })
                 .on("click", ".answerDownVoteBtn", function (e) {
                     e.preventDefault();
-                    if (socketModule.isConnected()) {
+                    isAuthenticated().then(() => {
                         let answerId = $(e.target)
                             .closest(".answer")
                             .attr("data-answer-id");
                         socketModule.downVoteAnswer(answerId);
-                    } else askToLogin();
+                    });
                 })
                 .on("click", ".deleteThreadBtn", function (e) {
                     e.preventDefault();
@@ -217,7 +224,7 @@ const gInterface = (function () {
                 })
                 .on("click", ".addTagBtn", function (e) {
                     e.preventDefault();
-                    if (socketModule.isConnected()) {
+                    isAuthenticated().then(() => {
                         let $currentThread = $(e.target).closest(".thread");
                         let threadId = $currentThread.attr("data-thread-id");
                         let threadQuestion = $currentThread.find(".question").text();
@@ -225,11 +232,10 @@ const gInterface = (function () {
                         $tagFormModal.find("input[name='threadId']").attr("value", threadId);
                         $tagFormModal.find(".threadQuestion").text(threadQuestion);
                         $tagFormModal.modal("show");
-                    } else askToLogin();
+                    });
                 })
-                .on('click','img', function(event) {
+                .on('click', 'img', function (event) {
                     let $image = $(this).attr('src');
-                    console.log($image);
                     let $imageModal = $("#imageModal");
                     $imageModal.find("img").html("");
                     $imageModal.find("img").attr("src", $image);
@@ -237,8 +243,9 @@ const gInterface = (function () {
                 });
             $("#askbutton").on("click", function (e) {
                 e.preventDefault();
-                if (socketModule.isConnected()) $("#questionFormModal").modal("show");
-                else askToLogin();
+                isAuthenticated().then(() => {
+                    $("#questionFormModal").modal("show");
+                });
             });
             $("#search_threads_on_tag_form").on("submit", function (e) {
                 e.preventDefault();
@@ -275,7 +282,7 @@ const gInterface = (function () {
             }).on('pasteText', function (ev, data) {
                 console.log("text: " + data.text);
             });
-            $('#btn-alias').on('click',function(e){
+            $('#btn-alias').on('click', function (e) {
                 e.preventDefault();
                 if (socketModule.isConnected()) $("#aliasFormModal").modal("show");
                 else askToLogin();
